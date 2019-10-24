f2caa5b4e3c74c95c701c93f47144211cb1a195b
Niall Grant <ngfgrant@gmail.com>
2019-10-24 12:49:42 +0100

    Add test

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -1,7 +1,7 @@
+from resolver.resolver import TestResolver
 
-class TestStackActions(object):
-    def setup_method(self, test_method):
-        pass
 
+class TestStackActions:
     def test_dummy_test(self):
-        pass
+        resolver = TestResolver()
+        assert resolver.resolve() == "TEST_RESOLVER_SUCCESS"
